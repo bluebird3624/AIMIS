f631798d2fbdca6b9f3ea48c0d10a61fd6e2a73a
Mary Wainaina <waynmary9@gmail.com>
2025-10-06 15:11:36 +0300

    Did migrations

---
+++ Interchée/backend/Interchée/Migrations/20251006084230_Added-submission-var.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Interchée.Migrations
+{
+    /// <inheritdoc />
+    public partial class Addedsubmissionvar : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "AssignmentId1",
+                table: "AssignmentSubmissions",
+                type: "bigint",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AssignmentSubmissions_AssignmentId1",
+                table: "AssignmentSubmissions",
+                column: "AssignmentId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AssignmentSubmissions_Assignments_AssignmentId1",
+                table: "AssignmentSubmissions",
+                column: "AssignmentId1",
+                principalTable: "Assignments",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AssignmentSubmissions_Assignments_AssignmentId1",
+                table: "AssignmentSubmissions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AssignmentSubmissions_AssignmentId1",
+                table: "AssignmentSubmissions");
+
+            migrationBuilder.DropColumn(
+                name: "AssignmentId1",
+                table: "AssignmentSubmissions");
+        }
+    }
+}
