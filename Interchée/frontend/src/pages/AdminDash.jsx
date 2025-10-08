d19964547c071fcaf431e9375f183587253c387d
Waffl3 <wafulanelson2004@gmail.com>
2025-10-08 09:11:27 +0300

    sidebar icons added

--- Interchée/frontend/src/pages/AdminDash.jsx
+++ Interchée/frontend/src/pages/AdminDash.jsx
@@ -3,19 +3,18 @@ import '../Styles/dashboards.css'
 import { useNavigate } from "react-router-dom";
 import { motion } from 'framer-motion';
 import  Group from '../assets/Group.svg';
-import { IoPersonOutline , IoPerson  } from "react-icons/io5";
+import { IoPersonOutline , IoPerson , IoPeopleOutline, IoPeople  } from "react-icons/io5";
+
 
 
 function AdminDash() {
 
+    const [isHovered, setIsHovered] = useState(false);
+    const [isActive, setIsActive] = useState(false);
 
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const ProfileshowSolid = isHovered || isActive;
+    const UsersshowSolid = isHovered || isActive;
 
-    const openMenu = () => setIsMenuOpen(true);
-    const closeMenu = () => setIsMenuOpen(false);
-
-    
-   
     return(
       <motion.div
             initial={{ x: '100%' }}
@@ -47,7 +46,34 @@ function AdminDash() {
             {/*  SIDEBAR */}
             <div className="sidebar">
               <div className="options-container">
-                <button className="sidebar-options"> Profile </button>
+
+
+                <button 
+                className="sidebar-options"
+                onMouseEnter={() => setIsHovered(true)}
+                onMouseLeave={() => setIsHovered(false)}
+                onMouseDown={() => setIsActive(true)}
+                onMouseUp={() => setIsActive(false)}
+                
+                > 
+                {ProfileshowSolid ?  <IoPerson/> : < IoPersonOutline />}  Profile 
+                
+                </button>
+
+                <button 
+                className="sidebar-options"
+                onMouseEnter={() => setIsHovered(true)}
+                onMouseLeave={() => setIsHovered(false)}
+                onMouseDown={() => setIsActive(true)}
+                onMouseUp={() => setIsActive(false)}
+                
+                > 
+                {UsersshowSolid ?  <IoPeople/> : < IoPeopleOutline />}  Users
+                
+                </button>
+
+                
+
 
 
               </div>
