ed783dda6f4187a429d948b470815befd728ab06
Waffl3 <wafulanelson2004@gmail.com>
2025-11-05 12:36:27 +0300

    the whole assignment module

---
+++ Interchée/frontend/src/components/AdminReviews.jsx
@@ -0,0 +1,15 @@
+import React, {useState, useEffect} from 'react';
+
+function Adminreviews (){
+
+    return(
+    <>
+    <h1> REVIIIIIIEW AAAAAAAAAAAdmin</h1>
+    </>
+
+    );
+
+}
+
+export default Adminreviews;
+
