dfac272fc45156bf7bfdaa6694139a560800e1bc
Mary Wainaina <waynmary9@gmail.com>
2025-11-10 10:52:12 +0300

    added dto to source control

---
+++ Interchée/backend/Interchée/Contracts/Assignments/AssignmentAssigneeReadDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Interchée.Contracts.Assignments
+{
+    public record AssignmentAssigneeReadDto(
+     Guid UserId,
+     string FirstName,
+     string LastName,
+     string Email,
+     string RoleName,
+     DateTime AssignedAt
+ );
+}
